575984d48cd4718aa09bdfb38bf1f7db8a26adae
giuseppe <you@example.com>
2019-04-26 10:17:36 +0200

    Update version 0.6

--- example.py
+++
@@ -1,5 +0,0 @@
-from os import path
-from pathlib import Path
-two_up = Path(__file__).resolve().parent
-
-print(two_up)
--- madyel/__init__.py
+++ madyel/__init__.py
@@ -1,2 +1,2 @@
 from .rtsp_curl import *
-__version__ = '0.5'
+__version__ = '0.6'
